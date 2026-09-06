a4a016e9ce800e387bb7f1f6f5a94fb79da1c2d6
aravind-govindhasamy <aravindapg06@gmail.com>
2026-09-06 13:04:51 +0530

    feat: configure Next.js static export and GitHub Pages deployment workflow

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,7 @@ import { withContentCollections } from "@content-collections/next";
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
+  trailingSlash: true,
   reactStrictMode: true,
   images: {
     unoptimized: true,
